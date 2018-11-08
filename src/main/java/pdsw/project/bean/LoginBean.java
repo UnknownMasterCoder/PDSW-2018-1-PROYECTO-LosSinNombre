1904a04bb205b808d7c01d2b9f6a62380a96ded3
Nicolas Cardenas <nicolas.cardenas@mail.escuelaing.edu.co>
2018-11-08 01:07:20 -0500

    pequeñas correcciones

--- src/main/java/pdsw/project/bean/LoginBean.java
+++ src/main/java/pdsw/project/bean/LoginBean.java
@@ -1,58 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pdsw.project.bean;
 
-import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
-import pdsw.project.entities.User;
 import pdsw.project.services.InitiativeBankServices;
 
-/**
- *
- * @author 2124203
- */
-    @ManagedBean (name = "loginBean")
+
+@ManagedBean (name = "loginBean")
+@RequestScoped
 public class LoginBean extends BasePageBean{
+    
+        @ManagedProperty(value = "#{param.contrasenia}")
 	private String contrasenia;
+    
+        @ManagedProperty(value = "#{param.mail}")
        	private String mail;
 
-
-        private 
-        static final long serialVersionUID = 35L;
+        private static final long serialVersionUID = 35L;
 
         @Inject
 	private InitiativeBankServices initiativeBankServices;
-
         
-    public LoginBean(){
-        
-    }    
-        
-    public String comprobacion(){
-        return mail;
-    }    
-        
-    //establecer y retornar la contraseña dada en el login   
-    public void setContrasenia(String contrasenia){
-        this.contrasenia = contrasenia;
-    }
-    
-    public String getContrasenia(){
+    public String getContrasenia() {
         return contrasenia;
     }
-    
-    //establecer y retornar la cuenta del mail dada en el login   
-    public void setMail(String mail){
-        this.mail = mail;
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
     }
-    
-    public String getMail(){
+
+    public String getMail() {
         return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 }
